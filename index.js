efacc861dd7fcccbe4bbabd0913b7ad768d9924c
Sushant Pandey <56975174+sushant-02@users.noreply.github.com>
2020-05-21 18:36:41 +0530

    Improved UI a little bit

--- index.js
+++ index.js
@@ -1,28 +1,87 @@
-function myFunction(el) {
-  var className = el.classList.item(0);
+const WINNING_CASES = {
+  'rock' : 'paper',
+  'paper' : 'scissors',
+  'scissors' : 'rock'
+}
 
-  document.querySelector('.game__triangle').style.display = 'none';
-  for(var i = 0; i < document.querySelectorAll('.icon').length; i++) {
-    document.querySelectorAll('.icon')[i].style.display = 'none';
+var score = 0;
+
+const userPicked = document.querySelectorAll('.user');
+
+userPicked.forEach((item) => {
+  document.querySelector('.header__score-value').textContent = score;
+  item.addEventListener('click', () => {
+    const userValue = item.children[1].name;
+    console.log(item);
+    removeDisplay();
+    item.classList.add('show-selected-user');    
+    changeStyles();
+    generateRandom(userValue, item);
+  })
+})
+
+const removeDisplay = () => {
+  document.querySelector('.game__triangle').classList.add('remove-display');
+  userPicked.forEach((item) => item.classList.add('remove-display'))
+}
+
+const changeStyles = () => {
+  document.querySelector('.game__h3-you').classList.add('show-display'); 
+  document.querySelector('.game__h3-house').classList.add('show-display');
+  document.querySelector('.game__h3-house').style.transition = "all .2s .5s";
+}
+
+const generateRandom = (userValue, item) => {
+  const items = document.querySelectorAll('.computer');
+  const random = items[Math.floor(Math.random() * 3)];
+  random.classList.add('show-selected-computer');
+  console.log(random);
+  var scoreCase = checkWin(userValue, random.children[1].name);
+  updateScore(scoreCase);
+  updateUI(scoreCase, item, random);
+}
+
+const checkWin = (userChoice, randomChoice) => {
+
+  if (userChoice === randomChoice) {
+    return ('draw');
+  } else if (WINNING_CASES[userChoice] === randomChoice) {
+    return ('lose');
+  } else {
+    return ('win');
+  }
+}
+
+const updateScore = (scoreCase) => {
+  if(scoreCase === 'win') {
+    score++;
+  } else if(scoreCase === 'lose') {
+    if(score > 0) {
+      score--;
+    } else {
+      score = 0;
+    }
+  }
+}
+
+const updateUI = (scoreCase, item, random) => {
+  document.querySelector('.header__score-value').textContent = score;
+  document.querySelector('.gameStat').classList.add('show-display');
+  document.querySelector('.btn-play').classList.add('show-display');
+  if(scoreCase === 'lose') {
+    document.querySelector('.btn-play').classList.add('lose');
+    document.querySelector('.gameStat').textContent = 'You Lose';
+  } else if(scoreCase === 'win') {
+    document.querySelector('.btn-play').classList.add('win');
+    document.querySelector('.gameStat').textContent = 'You Win';
+  } else {
+    document.querySelector('.btn-play').classList.add('win');
+    document.querySelector('.gameStat').textContent = 'Draw';
   }
 
-  document.querySelector('.game__h3-you').style.transition = 'all .2s .1s';
-  document.querySelector('.game__h3-you').style.opacity = '1';
-  document.querySelector('.game__h3-you').style.visibility = 'visible';
+  document.querySelector('.game__h3-you').classList.add('moveLeftText');
+  document.querySelector('.game__h3-house').classList.add('moveRightText');
 
-  document.querySelector('.' + className).style.display = 'block';
-  document.querySelector('.' + className).style.transition = 'transform .5s .1s';
-  document.querySelector('.' + className).style.transform = 'translate(-180%, 40%) scale(1.7)';
-
-  var cList = ['rock', 'paper', 'scissors'];
-  var index = Math.floor(Math.random() * 3);
-
-  document.querySelector('.game__icon-' + cList[index] + '-1').style.display = 'block';
-  document.querySelector('.game__icon-' + cList[index] + '-1').style.opacity = '1';
-  document.querySelector('.game__icon-' + cList[index] + '-1').style.visibility = 'visible';
-  document.querySelector('.game__icon-' + cList[index] + '-1').style.transition = 'all .2s .9s';
-
-  document.querySelector('.game__h3-house').style.transition = 'all .2s .1s';
-  document.querySelector('.game__h3-house').style.opacity = '1';
-  document.querySelector('.game__h3-house').style.visibility = 'visible';
+  item.classList.add('moveUser');
+  random.classList.add('moveHouse');
 }
